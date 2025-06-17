6df75466b9ee6ce819b78feaa34d616b4a978471
Alan Lima <alan.lima@ganil.com>
2025-06-17 14:48:36 +0200

    Sandbox: More Ethernet improvements

--- UDP.srcs/sources_1/bd/Box/hdl/Box_wrapper.vhd
+++ UDP.srcs/sources_1/bd/Box/hdl/Box_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2019.1 (win64) Build 2552052 Fri May 24 14:49:42 MDT 2019
---Date        : Tue Jun 17 10:56:41 2025
+--Date        : Tue Jun 17 14:37:47 2025
 --Host        : GANP1847 running 64-bit major release  (build 9200)
 --Command     : generate_target Box_wrapper.bd
 --Design      : Box_wrapper
--- UDP.srcs/sources_1/imports/sources_1/new/ethernet_generator.vhd
+++ UDP.srcs/sources_1/imports/sources_1/new/ethernet_generator.vhd
@@ -4,23 +4,31 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity ethernet_generator is
     Port ( 
-        clk : in std_logic;                         -- System clock input (125 MHz for Ethernet)
-        rst : in std_logic;                         -- Synchronous reset (active low)
+        clk : in std_logic;
+        rst : in std_logic;
         prtcl_on_off : in std_logic;
         src_mac : in std_logic_vector(47 downto 0);
         dst_mac : in std_logic_vector(47 downto 0);
         prtcl_type : in std_logic_vector (15 downto 0);
         data_in : in std_logic_vector(7 downto 0);
-        tready : in std_logic;                       -- Downstream ready signal (AXI-Stream handshake)
-        tdata : out std_logic_vector(7 downto 0);    -- Output data byte (AXI-Stream bus)
-        tvalid : out std_logic;                      -- Data valid signal (AXI-Stream handshake)
-        tlast : out std_logic;                       -- Indicates the last byte of a packet/frame (AXI-Stream)
+        tready : in std_logic;
+        tdata : out std_logic_vector(7 downto 0);
+        tvalid : out std_logic;
+        tlast : out std_logic;
         prtcl_en : out std_logic;
-        count : out std_logic_vector(5 downto 0)     -- Debug: byte counter (sent bytes)
+        start_dbg : out std_logic;
+        state_dbg : out std_logic_vector(3 downto 0);
+        count_dbg : out std_logic_vector(5 downto 0)
     );
 end ethernet_generator;
 
 architecture Behavioral of ethernet_generator is
+constant mac_length : integer := 6;
+constant type_field_length : integer := 2;
+constant padding_length : integer := 64;
+type mac_array is array (0 to mac_length-1) of std_logic_vector(7 downto 0);
+type type_array is array (0 to type_field_length-1) of std_logic_vector(7 downto 0);
+
 -- Begin: Timer component declaration
 component timer is
     Generic(
@@ -35,34 +43,48 @@ component timer is
 end component;
 -- End: Timer component declaration
 
--- Begin: Timer control signals
-signal en_timer_s : std_logic := '0'; -- Enables the timer counting process when set to '1'
-signal rst_timer_s : std_logic := '1';    -- Synchronous reset for the timer (active low)
-signal done_timer_s : std_logic;         -- Pulse output from the timer
--- End: Timer control signals
-
 -- FSM states
 type FSM is (
-    IDLE, 
+    WAITING,
+    START,
+    LOAD,
     DESTINATION_MAC, 
-    SOURCE_MAC, 
+    SOURCE_MAC,
     TYPE_FIELD,
-    ENABLE_IP,
-    DATA, 
+    ENABLE_PROTOCOL,
+    DATA,
     PADDING,
-    DONE
+    DONE,
+    RESET
 );
-signal state, next_state : FSM := IDLE;                                 -- Current and next state signals
+signal state, next_state : FSM := WAITING;
+type state_code_array is array (FSM) of std_logic_vector(3 downto 0);
 
-type mac_array is array (0 to 5) of std_logic_vector(7 downto 0);       -- MAC address type (6 Bytes)
-type type_array is array (0 to 1) of std_logic_vector(7 downto 0);      -- Protocol type (2 Bytes)
+constant state_code : state_code_array := (
+    WAITING         => "0001",
+    START           => "0010",
+    LOAD            => "0011",
+    DESTINATION_MAC => "0100",
+    SOURCE_MAC      => "0101",
+    TYPE_FIELD      => "0110",
+    ENABLE_PROTOCOL => "0111",
+    DATA            => "1000",
+    PADDING         => "1001",
+    DONE            => "1010",
+    RESET           => "1011"
+);
+
+-- Begin: Timer control signals
+signal en_timer_s : std_logic := '0';
+signal rst_timer_s : std_logic := '1';
+signal done_timer_s : std_logic;
+-- End: Timer control signals
 
 signal counter : integer := 0;
 signal dst_mac_s, src_mac_s : mac_array;
 signal prtcl_type_s : type_array;
 
 begin
-
 -- Begin: Timer instantiation
 inst_timer : timer generic map (125) port map (
     clk => clk,
@@ -77,7 +99,7 @@ process (clk)
 begin
     if (rising_edge(clk)) then
         if (rst = '0') then
-            state <= IDLE;
+            state <= RESET;
         else
             state <= next_state;
         end if;
@@ -90,35 +112,39 @@ end process;
 process (state, counter, tready)
 begin
     case state is
-        when IDLE =>
+        when WAITING =>
             if (done_timer_s = '1') then
-                next_state <= DESTINATION_MAC;
+                next_state <= START;
             else
-                next_state <= IDLE;
+                next_state <= WAITING;
             end if;
+        when START =>
+            next_state <= LOAD;
+        when LOAD =>
+            next_state <= DESTINATION_MAC;
         when DESTINATION_MAC =>
-            if (tready = '1' and counter = 5) then
+            if (tready = '1' and counter = mac_length-1) then
                 next_state <= SOURCE_MAC;
             else
                 next_state <= DESTINATION_MAC;
             end if;
         when SOURCE_MAC =>
-            if (tready = '1' and counter = 5) then
+            if (tready = '1' and counter = mac_length-1) then
                 next_state <= TYPE_FIELD;
             else
                 next_state <= SOURCE_MAC;
             end if;
         when TYPE_FIELD =>
-            if (tready = '1' and counter = 1) then
-                next_state <= ENABLE_IP;
+            if (tready = '1' and counter = type_field_length-1) then
+                next_state <= ENABLE_PROTOCOL;
             else
                 next_state <= TYPE_FIELD;
             end if;
-        when ENABLE_IP =>
+        when ENABLE_PROTOCOL =>
             if (tready = '1' and prtcl_on_off = '1') then
                 next_state <= DATA;
             else
-                next_state <= ENABLE_IP;
+                next_state <= ENABLE_PROTOCOL;
             end if;
         when DATA =>
             if (tready = '1' and prtcl_on_off = '0') then
@@ -127,15 +153,17 @@ begin
                 next_state <= DATA;
             end if;
         when PADDING =>
-            if (tready = '1' and counter = 63) then
+            if (tready = '1' and counter = padding_length-1) then
                 next_state <= DONE;
             else
                 next_state <= PADDING;
             end if;
         when DONE =>
-            next_state <= IDLE;
+            next_state <= WAITING;
+        when RESET =>
+            next_state <= WAITING;
         when others =>
-            next_state <= IDLE;        
+            next_state <= WAITING;        
     end case;
 end process;
 -- End: Next state logic
@@ -144,126 +172,134 @@ end process;
 process(clk)
 begin
     if (rising_edge(clk)) then
-        if (rst = '0') then
-            tvalid <= '0';
-            tdata <= (others => '0');
+        if (tready = '1') then
             tlast <= '0';
-            counter <= 0;
             prtcl_en <= '0';
-        elsif (tready = '1') then
+            start_dbg <= '1';
+            
             case state is
-                when IDLE =>
+                when WAITING =>                    
+                    tvalid <= '0';
+                    tdata <= (others => '0');
+                
+                when START =>
+                    tvalid <= '0';
+                    tdata <= (others => '0');
+                
+                when LOAD =>
                     dst_mac_s <= (dst_mac(47 downto 40), dst_mac(39 downto 32), dst_mac(31 downto 24), dst_mac(23 downto 16), dst_mac(15 downto 8), dst_mac(7 downto 0));
                     src_mac_s <= (src_mac(47 downto 40), src_mac(39 downto 32), src_mac(31 downto 24), src_mac(23 downto 16), src_mac(15 downto 8), src_mac(7 downto 0)); 
                     prtcl_type_s <= (prtcl_type(15 downto 8), prtcl_type(7 downto 0));
                     
-                    
                     tvalid <= '0';
                     tdata <= (others => '0');
-                    tlast <= '0';
-                    counter <= 0;
-                    prtcl_en <= '0';
-                    
+                
                 when DESTINATION_MAC =>
                     tvalid <= '1';
                     tdata <= dst_mac_s(counter);
-                    tlast <= '0';
-                    prtcl_en <= '0';
-                    
-                    if (counter = 5) then
-                        counter <= 0;
-                    else
-                        counter <= counter + 1;
-                    end if;
                     
                 when SOURCE_MAC =>
                     tvalid <= '1';
                     tdata <= src_mac_s(counter);
-                    tlast <= '0';
-                    prtcl_en <= '0';
-                    
-                    if (counter = 5) then
-                        counter <= 0;
-                    else
-                        counter <= counter + 1;
-                    end if;
                     
                 when TYPE_FIELD =>
                     tvalid <= '1';
                     tdata <= prtcl_type_s(counter);
-                    tlast <= '0';
-                    prtcl_en <= '0';
                     
-                    if (counter = 1) then
-                        counter <= 0;
-                    else
-                        counter <= counter + 1;
-                    end if;
-                    
-                when ENABLE_IP =>
+                when ENABLE_PROTOCOL =>
                     tvalid <= '0';
-                    tdata <= (others => '0');
-                    tlast <= '0';
                     prtcl_en <= '1';
-                    counter <= 0;
+                    tdata <= (others => '0');
                     
                 when DATA =>
                     tvalid <= '1';
-                    tdata <= data_in;
-                    tlast <= '0';
                     prtcl_en <= '1';
-                    counter <= 0;
+                    tdata <= data_in;
                     
                 when PADDING =>
                     tvalid <= '1';
                     tdata <= x"00";
-                    prtcl_en <= '0';
                     
-                    if (counter = 63) then
+                    if (counter = padding_length-1) then
                         tlast <= '1';
-                        counter <= 0;
-                    else
-                        counter <= counter + 1;
-                        tlast <= '0';
                     end if;
+                    
                 when DONE =>
                     tvalid <= '0';
                     tdata <= (others => '0');
-                    tlast <= '0';
-                    counter <= 0;
-                    prtcl_en <= '0';
+                    start_dbg <= '0';
+                    
+                when RESET =>
+                    tvalid <= '0';
+                    tdata <= (others => '0');
+                    start_dbg <= '0';
                     
                 when others =>
                     tvalid <= '0';
                     tdata <= (others => '0');
-                    tlast <= '0';
-                    counter <= 0;
-                    prtcl_en <= '0';
+                    start_dbg <= '0';
             end case;
         else
             tvalid <= '0';
             tdata <= (others => '0');
             tlast <= '0';
             prtcl_en <= '0';
+            
+            start_dbg <= '0';
         end if;
+        
+        state_dbg <= state_code(state);
     end if;
-
 end process;
 -- End: Output logic
 
+-- Begin: Timer logic
 process (state)
 begin
     case state is
-        when IDLE =>
+        when WAITING =>
             en_timer_s <= '1';
             rst_timer_s <= '1';
-        when OTHERS =>
+        when others =>
             en_timer_s <= '0';
             rst_timer_s <= '0';
      end case;
 end process;
+-- End: Timer logic
+
+-- Begin: Counter logic
+process (clk)
+begin
+    if (rising_edge(clk)) then
+        if (tready = '1') then
+            case state is
+                when DESTINATION_MAC | SOURCE_MAC =>
+                    if (counter = mac_length-1) then
+                        counter <= 0;
+                    else
+                        counter <= counter + 1;
+                    end if;
+                when TYPE_FIELD => 
+                    if (counter = type_field_length-1) then
+                        counter <= 0;
+                    else
+                        counter <= counter + 1;
+                    end if;
+                when PADDING =>
+                    if (counter = padding_length-1) then
+                        counter <= 0;
+                    else
+                        counter <= counter + 1;
+                    end if;
+                when others =>
+                    counter <= 0;
+            end case;
+        end if;
+    end if;
+end process;
+-- End: Counter logic
 
 -- Debug signal
-count <= std_logic_vector(to_unsigned(counter, 6));
+count_dbg <= std_logic_vector(to_unsigned(counter, 6));
 
 end Behavioral;
